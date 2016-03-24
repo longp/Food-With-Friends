26c0091b7b084d6499e91755f317d12404fb4109
Alexg2195 <alexg2195@gmail.com>
2016-03-24 14:53:25 -0400

    completed ejs makeover

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
+// Local Enviorment file loaded
 require('dotenv').config();
+
+
+// Load Modules
 
 var express = require("express");
 var expses = require('express-session')
 var bodyParser = require("body-parser");
 var logger = require('morgan');
+var path = require('path');
+
+
+// Local config modules
 
 var db = require('./config/connection.js');
 var passport = require('./config/passport.js');
+
+
+// Routing Modules
 
 var index = require('./routes/index.js');
 var authenticate = require('./routes/authenticate.js');
 var api = require('./routes/api.js');
 
+
+// Express Port Declaration
+
 var PORT = process.env.PORT || 3000;
 var app = express();
+
+
+// View Engine Setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
 // Middleware
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expses({
   secret: 'keyboard cat rocks',                                               //CHANGE FOR AN ENV VARIABLE LATER FOR PROD
@@ -42,10 +63,6 @@ app.use("/css", express.static("public/css"));
 app.use('/', index);
 app.use('/auth', authenticate);
 app.use('/api', api);
-
-app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-});
 
 
 // Connection to PORT
