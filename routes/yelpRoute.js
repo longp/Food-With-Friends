7e716b94e16fe6467b39905b39ff9f15c74093b1
long phan <lphan1989@gmail.com>
2016-03-31 17:09:58 -0400

    added yelp search to yelpRoute using static parameter

--- routes/yelpRoute.js
+++ routes/yelpRoute.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var yelp  = require('../config/yelp.js');
 
-// app.get('/yelp', function(req ,res ) {
-//   res.render('yelp')
-// })
+app.get('/yelp', function(req ,res ) {
+  yelp.search({
+    terms:"htodogs",
+    location:'Piscataway, NJg'
+  })
+})
 
 
 
