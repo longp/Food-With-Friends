b9e0fd56a5ab2c3463eb0800c75e6a0947cc9085
tammerg <tammergalal20@gmail.com>
2016-04-16 02:05:28 -0400

    still fix?

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/finalProjTest' || MONGODB_URI);
+mongoose.connect('mongodb://localhost/finalProjTest || MONGODB_URI');
 var db = mongoose.connection;
 
 module.exports = db;
