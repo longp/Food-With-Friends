d90dc1c58f0d4700260d628b3f3fec8448e83d3b
tammerg <tammergalal20@gmail.com>
2016-04-16 02:04:38 -0400

    still fix?

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://heroku_3rtj0nf1:cqiksmdodkhfujudh7oks0nhjp@ds011321.mlab.com:11321/heroku_3rtj0nf1');
+mongoose.connect('mongodb://localhost/finalProjTest' || MONGODB_URI);
 var db = mongoose.connection;
 
 module.exports = db;
