a21c5b8955bb0ec5bfd76a66eafd00d586532cef
Tammer Galal <tammergalal20@gmail.com>
2016-04-22 21:15:08 -0400

    progress on myaccounts page!

--- public/js/all.js
+++ public/js/all.js
@@ -262,6 +262,9 @@ app.controller('myaccountController', function($http, $scope){
       data:$scope.users
     }).success(function (users){
       console.log(users);
+      $scope.users.firstName = users.firstName;
+      $scope.users.lastName = users.lastName;
+      $scope.users.createdAt = users.createdAt;
     }).catch(function(err){
       console.log(err)
     })
