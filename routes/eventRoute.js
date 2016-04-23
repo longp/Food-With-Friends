36e2ec69fb0b38cbc94edf2fc91a02e31c2d0f9c
longp <lphan1989@gmail.com>
2016-04-23 08:57:36 -0400

    got the places and attndees to populate on page now, but the search algo for myevents still needs work

--- routes/eventRoute.js
+++ routes/eventRoute.js
@@ -15,33 +15,72 @@ router.post('/mine', function (req, res) {
   var username = req.user.username;
   var limit = parseInt(req.body.limit);
   var searchTerm = req.body.location;
-
+  if (searchTerm===undefined) {
+    searchTerm = false
+  }
+  console.log("searchtern" + searchTerm)
   geocoder.geocode({address:searchTerm, minConfidence: 0.5, limit: 5}, function(err, res) {
   })
   .then(function (location) {
+    console.log("searchtern" + searchTerm)
+
     stateLong = location[0].administrativeLevels.level1long;
     stateShort = location[0].administrativeLevels.level1short;
-    var searchLocation = stateLong+ ' ' + stateShort
-    Event.find({
-      $and :
-        [
-          {createdby:userId},
-            {$or:
-             [
-               {location:{$regex:stateLong,$options:"$i"}},
-              {location:{$regex:stateShort,$options:"$i"}},
-                ]
-            },
+    if (searchTerm) {
+      Event.find({
+        $and :[{createdby:userId},
+          {$or:[{location:{$regex:stateLong,$options:"$i"}},
+                {location:{$regex:stateShort,$options:"$i"}},]},
         ]
       })
-    .populate('places')
-    .populate('attendees')
-    .limit(limit)
-    .then(function (data) {
-      res.send(data);
-    })
+      .populate('places')
+      .populate('attendees')
+      .limit(limit)
+      .then(function (data) {
+        res.send(data);
+      })
+      console.log('the first one ran')
+    
+    } else {
+      Event.find({createdby:userId})
+      .populate('places')
+      .populate('attendees')
+      .limit(limit)
+      .then(function (data) {
+        console.log(data)
+        res.send(data);
+      })
+      console.log('the 2nd one ran')
+
+    }
   });
 })
 
+
+function findbyLocation (userId, limit, stateLong, stateShort) {
+  Event.find({
+    $and :[{createdby:userId},
+      {$or:[{location:{$regex:stateLong,$options:"$i"}},
+            {location:{$regex:stateShort,$options:"$i"}},]},
+    ]
+  })
+  .populate('places')
+  .populate('attendees')
+  .limit(limit)
+  .then(function (data) {
+    res.send(data);
+  })
+}
+
+function findAllAndPopulate (userId, limit) {
+  Event.find({createdby:userId})
+  .populate('places')
+  .populate('attendees')
+  .limit(limit)
+  .then(function (data) {
+    console.log(data)
+    res.send(data);
+  })
+}
 
 module.exports = router;
